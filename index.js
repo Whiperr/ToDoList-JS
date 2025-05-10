4c000dbbd175cbfcdd20288d547cf09eab3901c5
Marcin <marcin.karas2@wp.pl>
2025-05-10 15:36:57 +0200

    E 3/4

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const taskRoutes = require('./routes/taskRoutes');
 const errorHandler = require('./middleware/errorHandler');
-
+const path = require('path');
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api/tasks', taskRoutes);
@@ -14,3 +16,4 @@ app.use(errorHandler);
 // Start servera
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
